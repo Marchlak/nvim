1ce433d8fc34eaf1a0aadb0b9a801a4729649abc
Marchlak <bingor1@wp.pl>
2024-10-21 09:18:48 +0200

    nvim

--- init.lua
+++ init.lua
@@ -1,5 +1,15 @@
 require("config.keymaps")
 require("config.lazy")
 require("config.vim-options")
-vim.opt.clipboard:append("unnamedplus")
+vim.opt.clipboard = "unnamedplus"
+
+function CreateFileInCurrentDir()
+    local current_dir = vim.fn.expand('%:p:h')
+    local filename = vim.fn.input('Nazwa nowego pliku: ')
+    vim.cmd('edit ' .. current_dir .. '/' .. filename)
+end
+
+
+vim.api.nvim_set_keymap('n', '<leader>nf', ':lua CreateFileInCurrentDir()<CR>', { noremap = true, silent = true })
+
 
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -8,7 +8,11 @@ function plugin.config()
 		ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
 		sync_install = false,
 		auto_install = true,
-		highlight = { enable = true },
+    highlight = {
+			enable = true,
+			additional_vim_regex_highlighting = false,
+			disable = { "text" }, -- Wyłączenie podświetlania dla typu "Text"
+		},
 		indent = { enable = true },
 	})
 end
